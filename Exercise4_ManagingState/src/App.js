0dbb5e9c4e9f66ab305b46ec58a328d49678b7fb
root <goncholo@gmail.com>
2020-04-25 15:30:41 +0000

    Exercise done

--- Exercise4_ManagingState/src/App.js
+++ Exercise4_ManagingState/src/App.js
@@ -2,14 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const value1 = Math.floor(Math.random() * 100);
-const value2 = Math.floor(Math.random() * 100);
-const value3 = Math.floor(Math.random() * 100);
-const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
-
 class App extends Component {
+  
+  state = {
+    correct: 0,
+    answeredQuestions: 0,
+    value1: Math.floor(Math.random() * 100),
+    value2: Math.floor(Math.random() * 100),
+    value3: Math.floor(Math.random() * 100),
+    proposedAnswer: Math.floor(Math.random() * 3),
+  }
+
+newQuestion() {
+	this.setState(prevState => ({
+		value1: Math.floor(Math.random() * 100),
+		value2: Math.floor(Math.random() * 100),
+		value3: Math.floor(Math.random() * 100),
+		proposedAnswer: Math.floor(Math.random() * 3) + this.state.value1 + this.state.value2 + this.state.value3
+	}))
+}
+
+  isTheSumEqualToProposedAns() {
+    return this.state.value1 + this.state.value2 + this.state.value3 === this.state.proposedAnswer
+	//console.log(value1 + value2 + value3 === proposedAnswer)
+  }
+
+isTheAnswerCorrect(event) {
+  	const answer = event.target.name;
+
+	if ((answer === 'true' && this.isTheSumEqualToProposedAns) || (answer === 'false' && !this.isTheSumEqualToProposedAns)) {
+      console.log("Answer correct!")
+      	this.setState((previousState) => ({
+    		correct: previousState.correct + 1,
+      		answeredQuestions: previousState.answeredQuestions + 1
+    	}))
+    } else {
+      console.log("Answer incorrect... Please try again :D")
+    	this.setState((previousState) => ({
+    		correct: previousState.correct,
+      		answeredQuestions: previousState.answeredQuestions + 1
+    	}))
+    }
+
+this.newQuestion()
+}
+
   render() {
     return (
       <div className="App">
@@ -20,12 +57,12 @@ class App extends Component {
         <div className="game">
           <h2>Mental Math</h2>
           <div className="equation">
-            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+            <p className="text">{`${this.state.value1} + ${this.state.value2} + ${this.state.value3} = ${this.state.proposedAnswer}`}</p>
           </div>
-          <button>True</button>
-          <button>False</button>
+          <button onClick={(e) => this.isTheAnswerCorrect(e)} name="true">True</button>
+          <button onClick={(e) => this.isTheAnswerCorrect(e)} name="false">False</button>
           <p className="text">
-            Your Score: {numCorrect}/{numQuestions}
+            Your Score: {this.state.correct}/{this.state.answeredQuestions}
           </p>
         </div>
       </div>
